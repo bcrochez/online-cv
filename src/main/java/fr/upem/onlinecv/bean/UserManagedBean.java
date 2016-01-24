40c1a24b2cd073b50f94ab7a6ec230d1335c9450
bcrochez <bastien.crochez@gmail.com>
2016-01-24 11:09:12 +0100

    Add some tests

--- src/main/java/fr/upem/onlinecv/bean/UserManagedBean.java
+++ src/main/java/fr/upem/onlinecv/bean/UserManagedBean.java
@@ -1,63 +1,90 @@
-
 package fr.upem.onlinecv.bean;
 
 import java.io.Serializable;
+import javax.faces.context.FacesContext;
 
 /**
  *
  * @author Bastien
  */
 public class UserManagedBean implements Serializable {
-    
+
+    private static final long serialVersionUID = 1L;
+
     private Long id;
-    private String firstName;
-    private String lastName;
+    private String firstName = "Bastien";
+    private String lastName = "Crochez";
     private String email;
     private String password;
-    
+    private boolean isLogin = true;
+
     /**
      * Creates a new instance of UserManagedBean
      */
     public UserManagedBean() {
     }
-    
+
     public long getId() {
-	return id;
+        return id;
     }
 
     public void setId(long id) {
-	this.id = id;
+        this.id = id;
     }
 
     public String getFirstName() {
-	return firstName;
+        return firstName;
     }
 
     public void setFirstName(String name) {
-	this.firstName = name;
+        this.firstName = name;
     }
 
     public String getLastName() {
-	return lastName;
+        return lastName;
     }
 
     public void setLastName(String name) {
-	this.lastName = name;
+        this.lastName = name;
     }
 
     public String getEmail() {
-	return email;
+        return email;
     }
 
     public void setEmail(String email) {
-	this.email = email;
+        this.email = email;
     }
- 
+
     public String getPassword() {
-	return password;
+        return password;
     }
 
     public void setPassword(String password) {
-	this.password = password;
+        this.password = password;
+    }
+
+    public boolean isLogin() {
+        return isLogin;
+    }
+
+    public void isLogin(boolean b) {
+        isLogin = b;
+    }
+
+    public void logOut() {
+        if(isLogin) {
+            isLogin = false;
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.getApplication().getNavigationHandler().handleNavigation(context, null, "/index.xhtml?faces-redirect=true");
+        }
+
+    }
+
+    public void checkLogin() {
+        if (isLogin) {
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.getApplication().getNavigationHandler().handleNavigation(context, null, "/index.xhtml?faces-redirect=true");
+        }
     }
 }
--- src/main/java/fr/upem/onlinecv/model/User.java
+++ src/main/java/fr/upem/onlinecv/model/User.java
@@ -14,6 +14,7 @@ import javax.persistence.*;
 })
 
 public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
