8f44d5e25f2f30c6045a85603def8b261e6358cc
mdamis <mdamis@users.noreply.github.com>
2016-02-13 00:42:43 +0100

    Update SearchManagedBean to use a NamedQuery

--- src/main/java/fr/upem/onlinecv/bean/SearchManagedBean.java
+++ src/main/java/fr/upem/onlinecv/bean/SearchManagedBean.java
@@ -1,5 +1,12 @@
 package fr.upem.onlinecv.bean;
 
+import fr.upem.onlinecv.model.HibernateUtil;
+import fr.upem.onlinecv.model.UserEntity;
+import java.util.List;
+import javax.faces.context.FacesContext;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 /**
  *
  * @author Bastien
@@ -21,5 +28,22 @@ public class SearchManagedBean {
     public void setQuery(String query) {
         System.out.println("set query : "+query);
         this.query = query;
+    }
+    
+    public void search() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        
+        Query q = session.getNamedQuery("UserEntity.findByName");
+        q.setString("name", query);
+        List<UserEntity> users = q.list();
+        
+        session.close();
+        
+        for(UserEntity user : users) {
+            System.out.println(user);
+        }
+        
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getApplication().getNavigationHandler().handleNavigation(context, null, "/search.xhtml?faces-redirect=true");
     }
 }
