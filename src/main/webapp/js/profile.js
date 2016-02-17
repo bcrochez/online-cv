d7d4c01ff04d09cc2ce383bc0e5d3da6cc486e3b
bcrochez <bastien.crochez@gmail.com>
2016-02-17 12:06:40 +0100

    Add JS to hide forms

---
+++ src/main/webapp/js/profile.js
@@ -0,0 +1,16 @@
+var addWork;
+var addEducation;
+var addSkills;
+var addLanguages;
+
+window.onload = function() {
+    addWork = document.getElementById("add-work");
+    addEducation = document.getElementById("add-education");
+    addSkills = document.getElementById("add-skills");
+    addLanguages = document.getElementById("add-languages");
+};
+        
+        
+switchVisibility = function (div) {
+    div.hidden = !div.hidden;
+};
