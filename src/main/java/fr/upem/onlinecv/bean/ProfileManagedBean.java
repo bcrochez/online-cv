cf4373e9ad9130eff8eca010b3d6bf75ad534406
mdamis <mdamis@users.noreply.github.com>
2016-02-19 01:25:00 +0100

    Add ProfileManagedBean

---
+++ src/main/java/fr/upem/onlinecv/bean/ProfileManagedBean.java
@@ -0,0 +1,41 @@
+package fr.upem.onlinecv.bean;
+
+import fr.upem.onlinecv.model.HibernateUtil;
+import fr.upem.onlinecv.model.UserCv;
+import javax.faces.bean.RequestScoped;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author mdamis
+ */
+@RequestScoped
+public class ProfileManagedBean {
+    
+    private String userId;
+    private UserCv user;
+    
+    public ProfileManagedBean() {}
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    
+    public UserCv getUser() {
+        return user;
+    }
+
+    public void setUser(UserCv user) {
+        this.user = user;
+    }
+    
+    public void onload() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        user = (UserCv) (session.getNamedQuery("UserCv.findByUserId").setString("userId", userId).uniqueResult());   
+        session.close();
+    }
+}
