db61023a79a7d11367800ee0cfcc1def69606182
bcrochez <bastien.crochez@gmail.com>
2016-02-27 08:58:57 +0100

    Add editDescription

--- src/main/webapp/js/profile.js
+++ src/main/webapp/js/profile.js
@@ -12,6 +12,33 @@ window.onload = function () {
     addLanguages = document.getElementById("add-languages");
 };
 
+var editDescription = function () {
+    var description = document.getElementById("description");
+    var oldContent = description.innerHTML;
+    description.innerHTML = "";
+
+    var inputText = document.createElement("input");
+    inputText.type = "text";
+    inputText.placeholder = "Description";
+    description.appendChild(inputText);
+
+    var saveButton = createButton("Save", function () {
+        // TODO
+    });
+    description.appendChild(saveButton);
+
+    var cancelButton = createButton("Cancel", function () {
+        description.innerHTML = oldContent;
+    });
+    description.appendChild(cancelButton);
+};
+
+var createButton = function (innerHTML, onclickFunction) {
+    var newButton = document.createElement("button");
+    newButton.innerHTML = innerHTML;
+    newButton.onclick = onclickFunction;
+    return newButton;
+};
 
 var switchVisibility = function (div) {
     div.hidden = !div.hidden;
@@ -19,11 +46,17 @@ var switchVisibility = function (div) {
 
 var proficiencyIdToString = function (id) {
     switch (id) {
-        case 1: return "Beginner";
-        case 2: return "Conversational";
-        case 3: return "Business";
-        case 4: return "Fluent";
-        case 5: return "Native";
-        default : return "No proficiency";
+        case 1:
+            return "Beginner";
+        case 2:
+            return "Conversational";
+        case 3:
+            return "Business";
+        case 4:
+            return "Fluent";
+        case 5:
+            return "Native";
+        default :
+            return "No proficiency";
     }
 };
