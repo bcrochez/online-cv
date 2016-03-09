4a7ddb82e11d0db198d186b7dfe06226c911da72
mdamis <mdamis@users.noreply.github.com>
2016-03-09 21:40:25 +0100

    Add ajax for search

--- src/main/java/fr/upem/onlinecv/bean/SearchManagedBean.java
+++ src/main/java/fr/upem/onlinecv/bean/SearchManagedBean.java
@@ -17,8 +17,11 @@ import org.hibernate.Session;
  */
 public class SearchManagedBean {
 
+    private static final int LIMIT = 10;
+
     private String query;
-    private ArrayList<UserCv> users = new ArrayList();
+    private Set<UserCv> users = new HashSet<>();
+    private Set<UserCv> previewUsers = new HashSet<>();
 
     /**
      * Creates a new instance of SearchManagedBean
@@ -29,30 +32,46 @@ public class SearchManagedBean {
     public String getQuery() {
         return query;
     }
-    
+
     public void setQuery(String query) {
         this.query = query;
     }
-    
+
     public ArrayList<UserCv> getUsers() {
-        return users;
+        return new ArrayList<>(users);
     }
-    
+
+    public ArrayList<UserCv> getPreviewUsers() {
+        return new ArrayList<>(previewUsers);
+    }
+
     public void search() {
+        System.out.println("query " + query);
+
         users.clear();
-        
+        previewUsers.clear();
+
         List<String> tokens = query.length() != 0 ? Arrays.asList(query.split(" ")) : Collections.EMPTY_LIST; // FIXME if query is empty shows nothing
         Set<UserCv> userSet = new HashSet<>();
-        
+
         Session session = HibernateUtil.getSessionFactory().openSession();
-        for(String token : tokens) {
+        for (String token : tokens) {
             userSet.addAll(session.getNamedQuery("UserCv.findByName").setString("name", "%" + token + "%").list());
-        }     
+        }
         session.close();
-   
+
         users.addAll(userSet);
-        query = "";
-        
+
+        if (users.size() > LIMIT) {
+            ArrayList<UserCv> userList = new ArrayList<>(users);
+            previewUsers = new HashSet<>(userList.subList(0, LIMIT));
+        } else {
+            previewUsers = users;
+        }
+    }
+
+    public void searchAndNavigate() {
+        search();
         FacesContext context = FacesContext.getCurrentInstance();
         context.getApplication().getNavigationHandler().handleNavigation(context, null, "/search.xhtml?faces-redirect=true");
     }
